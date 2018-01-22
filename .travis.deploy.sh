90c7042438b8236c961cb14028b620139bbbc830
Justin Harringa <jharringa@salesforce.com>
2018-01-22 13:36:57 -0800

    Add curly braces around HOME in .travis.deploy.sh

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cp .travis.settings.xml $HOME/.m2/settings.xml
+cp .travis.settings.xml ${HOME}/.m2/settings.xml
 mvn releaser:release
 docker login -u="${DOCKER_USER}" -p="${DOCKER_PASSWORD}";
 docker push salesforce/dockerfile-image-update
