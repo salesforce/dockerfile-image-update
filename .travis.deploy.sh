aca6585d2a53d3fedb64b3a2a0bf9f34954311ad
Andrey Falko <afalko@users.noreply.github.com>
2018-07-18 17:15:53 -0700

    Use shred to also remove

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -11,7 +11,7 @@ openssl aes-256-cbc -K $encrypted_00fae8efff8c_key -iv $encrypted_00fae8efff8c_i
 gpg --fast-import codesigning.asc
 
 # Remove code signing keys (since the releaser plugin requires a clean git workspace)
-shred codesigning.asc && rm codesigning.asc
+shred --remove codesigning.asc
 
 # Set up Maven settings and release
 cp .travis.settings.xml ${HOME}/.m2/settings.xml
