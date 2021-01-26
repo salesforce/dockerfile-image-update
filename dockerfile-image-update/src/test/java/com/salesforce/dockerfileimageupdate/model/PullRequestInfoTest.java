74fd1b8afe81e43e74e8fa5c13dc241d1f2ade95
Stephen Connolly <stephen.alan.connolly@gmail.com>
2021-01-26 09:03:12 +0000

    Add support for extra body content in PRs

--- dockerfile-image-update/src/test/java/com/salesforce/dockerfileimageupdate/model/PullRequestInfoTest.java
+++ dockerfile-image-update/src/test/java/com/salesforce/dockerfileimageupdate/model/PullRequestInfoTest.java
@@ -10,35 +10,49 @@ public class PullRequestInfoTest {
     public static final String IMAGE = "myimage";
     public static final String TAG = "tag";
     public static final String TITLE = "title";
+    public static final String EXTRA = "extra";
 
     @Test
     public void testGetBody() {
         String imageAndTag = IMAGE + ":" + TAG;
-        PullRequestInfo pullRequestInfo = new PullRequestInfo(TITLE, IMAGE, TAG);
+        PullRequestInfo pullRequestInfo = new PullRequestInfo(TITLE, IMAGE, TAG, null);
         assertEquals(pullRequestInfo.getBody(), String.format(BODY_TEMPLATE, IMAGE, IMAGE, TAG, imageAndTag));
     }
 
     @Test
+    public void testGetBodyWithExtra() {
+        String imageAndTag = IMAGE + ":" + TAG;
+        PullRequestInfo pullRequestInfo = new PullRequestInfo(TITLE, IMAGE, TAG, EXTRA);
+        assertEquals(pullRequestInfo.getBody(), String.format(BODY_TEMPLATE, IMAGE, IMAGE, TAG, imageAndTag) + '\n' + EXTRA);
+    }
+
+    @Test
     public void testGetBodyIsOldConstantIfImageAndTagNull() {
-        PullRequestInfo pullRequestInfo = new PullRequestInfo(TITLE, null, null);
+        PullRequestInfo pullRequestInfo = new PullRequestInfo(TITLE, null, null, null);
         assertEquals(pullRequestInfo.getBody(), OLD_CONSTANT_BODY);
     }
 
     @Test
+    public void testGetBodyIsOldConstantWithExtraIfExtraSuppliedAndImageAndTagNull() {
+        PullRequestInfo pullRequestInfo = new PullRequestInfo(TITLE, null, null, EXTRA);
+        assertEquals(pullRequestInfo.getBody(), OLD_CONSTANT_BODY + '\n' + EXTRA);
+    }
+
+    @Test
     public void testGetTitle() {
-        PullRequestInfo pullRequestInfo = new PullRequestInfo(TITLE, IMAGE, TAG);
+        PullRequestInfo pullRequestInfo = new PullRequestInfo(TITLE, IMAGE, TAG, null);
         assertEquals(pullRequestInfo.getTitle(), TITLE);
     }
 
     @Test
     public void testGetDefaultTitleIfNull() {
-        PullRequestInfo pullRequestInfo = new PullRequestInfo(null, IMAGE, TAG);
+        PullRequestInfo pullRequestInfo = new PullRequestInfo(null, IMAGE, TAG, null);
         assertEquals(pullRequestInfo.getTitle(), DEFAULT_TITLE);
     }
 
     @Test
     public void testGetDefaultTitleIfEmpty() {
-        PullRequestInfo pullRequestInfo = new PullRequestInfo("   ", IMAGE, TAG);
+        PullRequestInfo pullRequestInfo = new PullRequestInfo("   ", IMAGE, TAG, null);
         assertEquals(pullRequestInfo.getTitle(), DEFAULT_TITLE);
     }
 }
